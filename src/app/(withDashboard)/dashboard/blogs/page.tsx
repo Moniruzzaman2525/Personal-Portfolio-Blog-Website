3facec136e1dc2a2c303e46a2e6507302de3ba5a
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-02-11 07:21:32 +0600

    get blogs api implement done

--- src/app/(withDashboard)/dashboard/blogs/page.tsx
+++ src/app/(withDashboard)/dashboard/blogs/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
 
-const ListsBlogs = () => {
+
+const ListsBlogs = async () => {
+
+    const res = await fetch('http://localhost:5000/api/blogs')
+    console.log(await res.json())
+
+
+
+
     return (
-        <div>
-            Lists all blogs
+        <div className="p-6 bg-white shadow-md rounded-lg">
+            <h2 className="text-xl font-bold mb-4">All Blogs</h2>
+
         </div>
     );
 };
 
 export default ListsBlogs;
+
